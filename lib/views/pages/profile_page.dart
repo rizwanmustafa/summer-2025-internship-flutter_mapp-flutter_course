7dd2f82fb8b0a7b9b7688a6dc817996cce176902
Rizwan Khan <rizwanmustafa0000@gmail.com>
2025-07-02 15:38:01 +0500

    feat: completed till end of switchs

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool? isChecked = false;
+  bool isSwitched = false;
+
+  TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile Page!'));
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            controller: controller,
+            onEditingComplete: () => setState(() {}),
+          ),
+          Text(controller.text),
+          Checkbox(
+            value: isChecked,
+            onChanged: (bool? newVal) => setState(() {
+              isChecked = newVal;
+            }),
+          ),
+
+          CheckboxListTile(
+            title: Text('Click me!'),
+            value: isChecked,
+            onChanged: (bool? newVal) => setState(() {
+              isChecked = newVal;
+            }),
+          ),
+
+          Switch(
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+
+          SwitchListTile(
+            title: Text('Switch me!'),
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
