bd918236125c5782ff63d5221150a620e1989398
Rizwan Khan <rizwanmustafa0000@gmail.com>
2025-07-02 15:59:54 +0500

    feat: completed till inkwell widgte

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -10,54 +10,79 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   bool? isChecked = false;
   bool isSwitched = false;
+  double sliderValue = 0;
 
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            controller: controller,
-            onEditingComplete: () => setState(() {}),
-          ),
-          Text(controller.text),
-          Checkbox(
-            value: isChecked,
-            onChanged: (bool? newVal) => setState(() {
-              isChecked = newVal;
-            }),
-          ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              controller: controller,
+              onEditingComplete: () => setState(() {}),
+            ),
+            Text(controller.text),
+            Checkbox.adaptive(
+              value: isChecked,
+              onChanged: (bool? newVal) => setState(() {
+                isChecked = newVal;
+              }),
+            ),
 
-          CheckboxListTile(
-            title: Text('Click me!'),
-            value: isChecked,
-            onChanged: (bool? newVal) => setState(() {
-              isChecked = newVal;
-            }),
-          ),
+            CheckboxListTile.adaptive(
+              title: Text('Click me!'),
+              value: isChecked,
+              onChanged: (bool? newVal) => setState(() {
+                isChecked = newVal;
+              }),
+            ),
 
-          Switch(
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
+            Switch.adaptive(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
 
-          SwitchListTile(
-            title: Text('Switch me!'),
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-        ],
+            SwitchListTile.adaptive(
+              title: Text('Switch me!'),
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+
+            Slider.adaptive(
+              max: 10,
+              divisions: 10,
+              value: sliderValue,
+              onChanged: (newValue) => setState(() {
+                sliderValue = newValue;
+              }),
+            ),
+            InkWell(
+              splashColor: Colors.green,
+              onTap: () {
+                print('Background Image selected!');
+              },
+
+              child: Container(
+                height: 200,
+                color: Colors.white12,
+                width: double.infinity,
+                child: Image.asset('assets/images/wallpaper2.jpg'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
