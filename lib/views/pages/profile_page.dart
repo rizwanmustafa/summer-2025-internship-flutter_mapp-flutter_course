c9eeb19ded59191059de006a2da5cc0bd325bfaa
Rizwan Khan <rizwanmustafa0000@gmail.com>
2025-07-02 16:40:02 +0500

    feat: completed till the end of the chapter 6

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,6 +13,8 @@ class _ProfilePageState extends State<ProfilePage> {
   bool isSwitched = false;
   double sliderValue = 0;
 
+  String dropdownButtonValue = 'PKR';
+
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -20,6 +23,20 @@ class _ProfilePageState extends State<ProfilePage> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
+            DropdownButton(
+              value: dropdownButtonValue,
+              items: [
+                DropdownMenuItem(value: 'PKR', child: Text('PKR')),
+                DropdownMenuItem(value: 'USD', child: Text('USD')),
+                DropdownMenuItem(value: 'AED', child: Text('AED')),
+                DropdownMenuItem(value: 'SAR', child: Text('SAR')),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  dropdownButtonValue = value as String;
+                });
+              },
+            ),
             TextField(
               decoration: InputDecoration(border: OutlineInputBorder()),
               controller: controller,
@@ -75,11 +92,30 @@ class _ProfilePageState extends State<ProfilePage> {
               },
 
               child: Container(
-                height: 200,
+                height: 50,
                 color: Colors.white12,
                 width: double.infinity,
                 child: Image.asset('assets/images/wallpaper2.jpg'),
               ),
+            ),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click Me!'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.greenAccent,
+                foregroundColor: Colors.white,
+              ),
+            ),
+
+            ElevatedButton(onPressed: () {}, child: Text('Click Me!')),
+            FilledButton(onPressed: () {}, child: Text('Click Me!')),
+            TextButton(onPressed: () {}, child: Text('Click Me!')),
+            CloseButton(),
+            BackButton(),
+            CupertinoButton.filled(
+              child: Text('Click Me! I am an iOS Button'),
+              onPressed: () {},
             ),
           ],
         ),
