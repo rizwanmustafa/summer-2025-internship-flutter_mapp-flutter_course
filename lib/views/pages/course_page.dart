9f4a493b3f91fab85a70a0dc0506efd20fb5bcf1
Rizwan Khan <rizwanmustafa0000@gmail.com>
2025-07-10 12:39:08 +0500

    feat: completed till the start of chatpter 12

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/data/classes/activity_class.dart';
 import 'package:flutter_tutorial/views/widgets/hero_widget.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  late Future<Activity> activity;
+  @override
+  void initState() {
+    activity = getData();
+    super.initState();
+  }
+
+  Future<Activity> getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      final activity = Activity.fromJson(
+        convert.jsonDecode(response.body) as Map<String, dynamic>,
+      );
+
+      debugPrint(activity.activity);
+
+      return activity;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load an activity!');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(children: [HeroWidget(title: 'Course Page')]),
-        ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          Widget retWidget;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            retWidget = Center(child: CircularProgressIndicator.adaptive());
+          } else if (snapshot.hasData) {
+            Activity activity = snapshot.data as Activity;
+            retWidget = Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroWidget(title: activity.activity),
+                    Text(activity.activity),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            retWidget = Center(child: Text('Error!'));
+          }
+
+          return retWidget;
+        },
       ),
     );
   }
